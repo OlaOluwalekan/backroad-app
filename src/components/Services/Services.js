98fbc67ca53dfb49556285b9297f707e3d44bde9
OlaOluwalekan <olalekanbello534@gmail.com>
2023-01-10 12:20:54 +0100

    adjusted services area

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,18 +1,18 @@
-import "./Services.css";
-import SectionHead from "../SectionHead";
-import Service from "./Service";
-import { servicesData } from "../data";
+import "./Services.css"
+import SectionHead from "../SectionHead"
+import Service from "./Service"
+import { servicesData } from "../data"
 
 export default function Services() {
   return (
-    <section className="services" id="services">
-      <SectionHead firstText="our" secondText="services" />
-      <article className="service-list">
+    <section className='services' id='services'>
+      <SectionHead firstText='our' secondText='services' />
+      <article className='service-list'>
         {servicesData.map((service) => {
-          const { id, icon, name } = service;
-          return <Service key={id} icon={icon()} name={name} />;
+          const { id, icon, name } = service
+          return <Service key={id} icon={icon()} name={name} />
         })}
       </article>
     </section>
-  );
+  )
 }
